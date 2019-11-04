51a29a68b4d6e781f7c19866f1d01f57fe716010
birme <jonas.birme@eyevinn.se>
2019-11-04 11:04:20 +0100

    Added the ability to add DSPs on the command line with env params

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const SSPEngine = require('./index.js');
 
 const engine = new SSPEngine();
-engine.addProvider({
+if (process.env.DSPS) {
+  let providers = process.env.DSPS.split(',');
+  let providerId = 1;
+  providers.forEach(provider => {
+    engine.addProvider({
+      name: `DSP ${providerId++}`,
+      endpoint: provider,
+      openRtbVersion: "2.3"
+    });
+  });
+} else {
+  engine.addProvider({
+      name: "Mock DSP 1",
+      endpoint: "http://localhost:8081/dsp?mock=1",
+      openRtbVersion: "2.3",
+    });
+  engine.addProvider({
     name: "Mock DSP 1",
     endpoint: "http://localhost:8081/dsp?mock=1",
     openRtbVersion: "2.3",
   });
-engine.addProvider({
-  name: "Mock DSP 1",
-  endpoint: "http://localhost:8081/dsp?mock=1",
-  openRtbVersion: "2.3",
-});
+}
 engine.listen(process.env.PORT || 8000);
