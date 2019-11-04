0a80dd419ac236a2fabb72f95d7373137f608f38
birme <jonas.birme@eyevinn.se>
2019-11-04 10:17:46 +0100

    Use the standalone dsp project

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const SSPEngine = require('./index.js');
 const engine = new SSPEngine();
 engine.addProvider({
     name: "Mock DSP 1",
-    endpoint: "http://localhost:8081/dsp",
+    endpoint: "http://localhost:8081/dsp?mock=1",
     openRtbVersion: "2.3",
   });
 engine.addProvider({
   name: "Mock DSP 1",
-  endpoint: "http://localhost:8081/dsp",
+  endpoint: "http://localhost:8081/dsp?mock=1",
   openRtbVersion: "2.3",
 });
 engine.listen(process.env.PORT || 8000);
